d97b59ba258bfae64f2ad15af068f4b2806cb21f
Ivan <ivan.kyselov.ukr@online.ua>
2016-03-24 14:40:59 +0200

    Initial commit

---
+++ app/src/main/java/ua/kyselov/android/runtimer/StopWatchActivity.java
@@ -0,0 +1,59 @@
+package ua.kyselov.android.runtimer;
+
+import android.os.HandlerThread;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.os.Handler;
+
+
+
+public class StopWatchActivity extends AppCompatActivity {
+    private int seconds = 0;
+    private boolean running;
+    TextView timeView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stop_watch);
+        timeView = (TextView)findViewById(R.id.timerTxtV);
+        runTimer();
+    }
+
+    public void onTimerStart(View view) {
+        running = true;
+    }
+
+    public void onTimerStop(View view) {
+        running = false;
+    }
+
+    public void onTimerReset(View view) {
+        running = false;
+        seconds = 0;
+    }
+
+    private void runTimer(){
+    final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds / 3600;
+                int minutes = (seconds % 3600) / 60;
+                int secs = seconds % 60;
+
+                String time = String.format("%d:%02d:%02d",
+                        hours, minutes, secs);
+                timeView.setText(time);
+                if (running) {
+                    seconds++;
+                }
+                handler.postDelayed(this, 1000);
+            }
+        });
+
+    }
+}
+
